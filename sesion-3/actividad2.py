938e3714e53e33a12bf91f856cf01ea955d45b9d
Agustin <105565517+agustinwaizman@users.noreply.github.com>
2022-06-23 11:14:31 -0300

    Add files via upload

---
+++ sesion-3/actividad2.py
@@ -0,0 +1,6 @@
+peso = float(input("Ingrese su peso en kg: "))
+estatura = float(input("Ingrese su estatura en metros: "))
+
+imc= peso / estatura
+
+print(f"Tu indece de masa corporal es de {imc}")
