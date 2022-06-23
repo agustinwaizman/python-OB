c273822d3e128d5201050015103c60e88b1007a3
Agustin <105565517+agustinwaizman@users.noreply.github.com>
2022-06-23 11:09:33 -0300

    Update actividad1.py

--- sesion-3/actividad1.py
+++ sesion-3/actividad1.py
@@ -7,3 +7,13 @@ Casado = False
 Con_Hijos = False
 Lista_de_amigos = ["Matias", "Ulises", "Miguel", "Lautaro", "Lucas"]
 Peliculas_vistas = { 1: "El protegido", 2: "Fragmentado", 3: "Glass"}
+
+print(Nombre)
+print(Apellido)
+print(Edad)
+print(Email)
+print(Telefono)
+print(Casado)
+print(Con_Hijos)
+print(Lista_de_amigos)
+print(Peliculas_vistas)
