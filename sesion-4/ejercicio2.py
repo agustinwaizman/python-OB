e7b3581032176735aed3c6171cd4296d7d346c72
Agustin <105565517+agustinwaizman@users.noreply.github.com>
2022-06-23 13:36:50 -0300

    Update ejercicio2.py

--- sesion-4/ejercicio2.py
+++ sesion-4/ejercicio2.py
@@ -1,6 +1,9 @@
 numero_inicial = int(input("ingrese el numero inicial: "))
 numero_final = int(input("ingrese el numero final: "))
+numeros_impares: [int] = []
 
 for i in range(numero_inicial, numero_final):
-    if (i % 2) == 1:
-        print(i)
+    if (i % 2) != 0:
+        numeros_impares.append(i)
+print(f"esta es la lista de numeros impares entre {numero_inicial} y {numero_final}:")
+print(numeros_impares)
